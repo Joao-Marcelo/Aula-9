3d22eaa0895d83997fe5ae23c1694e6a65ec2b55
Joao-Marcelo <97107881+Joao-Marcelo@users.noreply.github.com>
2022-01-04 11:15:35 -0300

    Primeiro envio

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var square;
+
+function setup() {
+  createCanvas(400,400);
+  square = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+if (keyDown("RIGHT")){
+square.x = square.x + 3;
+ background("purple");
+}
+}
+
+
+
+
